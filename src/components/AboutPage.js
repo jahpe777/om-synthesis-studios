c82c0ab173f571a2b2935ede26b847d47ee71b97
James Higgs <jameshiggs@Jamess-MacBook-Pro.local>
2024-05-17 17:30:47 -0700

    Added YouTube link to About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,7 +6,7 @@ function AboutPage() {
       <div className="about-banner"></div>
       <h1>About Om Synthesis Studios</h1>
       <p>
-        Om Synthesis Studios is a YouTube channel dedicated to providing you with a transformative and immersive experience through the power of meditative music and visuals. Our channel is a sanctuary for those seeking to unwind, find inner peace, and embark on a journey of self-discovery.
+        Om Synthesis Studios is a <a href="https://www.youtube.com/channel/UC3MC2lsf8ritNB6iaG1-zvw" target="_blank" rel="noopener noreferrer">YouTube</a> channel dedicated to providing you with a transformative and immersive experience through the power of meditative music and visuals. Our channel is a sanctuary for those seeking to unwind, find inner peace, and embark on a journey of self-discovery.
       </p>
       <p>
         We specialize in creating original, high-quality music that combines the ancient wisdom of meditation with modern, innovative soundscapes. Our videos feature a diverse range of meditative music all designed to help you relax, reduce stress, and cultivate a sense of well-being.
