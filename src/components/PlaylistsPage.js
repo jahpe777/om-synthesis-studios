3299f43a5584efc7b13632eeecd110b568ac6f64
James Higgs <jameshiggs@Jamess-MacBook-Pro.local>
2024-05-24 16:34:54 -0700

    updated playlists

--- src/components/PlaylistsPage.js
+++ src/components/PlaylistsPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 const playlists = [
-  { title: '60 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWZ4MrR9ITL1Ec5ojaRNcArX&si=KrEUEAeGik8G7yT-' },
-  { title: '45 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWb_PjRby32B8Sx5dCkVo436&si=sm7K_K7osjOAqJcQ' },
-  { title: '30 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWZNSCpV0_72qpp2k6SaSHUX&si=WMFgIKLYZRiOAFt0' },
-  { title: '20 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWZR9_lIGxpzJOFsCpuVD3qg&si=Gd7ZHk0jR-V9KonD' },
-  { title: '15 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWZ-Oc1Yx9qADGebsjDT9IiU&si=ArTefa7HOawRoMn8' },
-  { title: '10 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWaZoRPxk3ucgx094NJEACj8&si=5ZP45_hSfYaKsOL3' },
-  { title: '5 Minute Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWboZK_5quvnJpWsowDjeHCy&si=TR9_teVDCVjum4gk' },
+  { title: 'COSMIC Series - Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWaZoRPxk3ucgx094NJEACj8&si=K3BZ1fVVF5TfutGN' },
+  { title: 'DESERT Series - Meditation Music', youtube_link: 'https://youtube.com/playlist?list=PLJfvMQgAiaWZNSCpV0_72qpp2k6SaSHUX&si=MM1ik9mRylVh1A6n' },
 ];
 
 function PlaylistsPage() {
